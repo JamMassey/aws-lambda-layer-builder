e6103d092af45502d9159b78a107b2f55c2fad0e
James Massey <33434582+JamMassey@users.noreply.github.com>
2024-01-12 07:07:32 +0000

    Dev (#23)
    * increment version number
    
    * feat: remove version limitation of python (#17)
    
    * increment version number (#15)
    
    * tweak: remove version number check
    
    * fix: change to sam build images
    
    * fix: change to public ECR repos
    
    ---------
    
    Co-authored-by: James Massey <33434582+JamMassey@users.noreply.github.com>
    Co-authored-by: Phyo <phyoakl@hexcode.tech>
    
    * fix: Update (#20)
    
    * updating package v
    
    * updating package v
    
    * Versioneer (#22)
    
    * added gh actions workflows to aid in semantic versioning
    
    * update README
    
    * Versioneer (#24)
    
    * added gh actions workflows to aid in semantic versioning
    
    * update README
    
    * dependency update
    
    ---------
    
    Co-authored-by: Phyo Arkar Lwin <phyo.arkarlwin@gmail.com>
    Co-authored-by: Phyo <phyoakl@hexcode.tech>

--- setup.py
+++ setup.py
@@ -1,44 +1,44 @@
 import setuptools
 
+import versioneer
+
 setuptools.setup(
-    name='pylayerize',
-    keywords='aws lambda layer pylayerize pylayerizer pylayerize',
-    description='pylayerize simplifies the process of creating AWS Lambda Layers by leveraging Docker to compile and package Python dependencies for any targeted runtime. With support for custom packages and private GitHub repos, pylayerize optimizes the creation and deployment of Lambda Layers for improved development efficiency.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    package_dir={'': 'src'},
-    package_data={
-        'pylayerize': ['dockerfiles/*']
-    },
+    name="pylayerize",
+    keywords="aws lambda layer pylayerize pylayerizer pylayerize",
+    description="pylayerize simplifies the process of creating AWS Lambda Layers by leveraging Docker to compile and package Python dependencies for any targeted runtime. With support for custom packages and private GitHub repos, pylayerize optimizes the creation and deployment of Lambda Layers for improved development efficiency.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    package_data={"pylayerize": ["dockerfiles/*"]},
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     include_package_data=True,
     install_requires=[],
-    packages=setuptools.find_packages(where='src'),
+    packages=setuptools.find_packages(where="src"),
     entry_points={
-        'console_scripts': [
-            'pylayerize=pylayerize.__main__:main',
+        "console_scripts": [
+            "pylayerize=pylayerize.__main__:main",
         ],
     },
     classifiers=[
         # see https://pypi.org/classifiers/
-        'Development Status :: 5 - Production/Stable',
-
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-	python_requires='>=3.7',
+    python_requires=">=3.7",
     extras_require={
-        'dev': ['check-manifest'],
+        "dev": ["check-manifest", "versioneer"],
         # 'test': ['coverage'],
     },
-    author='James Massey',
-    author_email='jammassey@hotmail.com',
-    url='https://github.com/JamMassey/lambda-layer-builder',
+    author="James Massey",
+    author_email="jammassey@hotmail.com",
+    url="https://github.com/JamMassey/lambda-layer-builder",
 )
